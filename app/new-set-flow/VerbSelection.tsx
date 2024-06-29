a7b4c2239f0a0ec76dbd3b68dec83edbd6862d27
Clement Perrier <clement.perrier25@gmail.con>
2024-06-29 16:39:10 +0200

    Verbs display v1

--- app/new-set-flow/VerbSelection.tsx
+++ app/new-set-flow/VerbSelection.tsx
@@ -1,18 +1,41 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, FlatList } from 'react-native';
 import { Button } from 'react-native';
 import { useAppNavigation } from '@/hooks/useAppNavigation';
 import { useAppSelector } from '@/state/hooks';
-import { TenseState } from '@/state/interfaces/TenseState';
-import { RootState } from '@/state/store';
+import { Verbs } from '@/constants/Verbs';
+
 export default function VerbSelection() {
 
   const navigation = useAppNavigation();
 
   const selectedTense = useAppSelector(state => state.selectedTense.value)
 
+  const verbList = Verbs.map(verb => 
+    <View key={verb.id} style={styles.button}>
+      <Button title={verb.name}></Button>
+    </View>
+  )
+
   return (
     <View style={styles.container}>
       { selectedTense && <Text>{selectedTense.name}</Text> }
+      <FlatList 
+        contentContainerStyle={{
+          marginHorizontal: '10%',
+          // alignItems: "stretch",
+          // width: '100%'
+        }}
+        ItemSeparatorComponent={() => <View style={{height: 20}} />}
+        numColumns={3}
+        columnWrapperStyle={{
+          justifyContent: 'space-between',
+          alignContent: 'center',
+          width: '100%'
+        }}
+        data={Verbs}
+        renderItem={({item}) => <Button title={item.name}></Button>}
+        >
+      </FlatList>
       <Button 
         title='ADD TO SET'
         onPress={() => navigation.navigate('Set summary')}
@@ -27,4 +50,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  button: {
+    marginBottom: 20,
+    width: 250
+  }
 });
---
+++ constants/Verbs.ts
@@ -0,0 +1,498 @@
+export const Verbs = [
+    {
+        name: "surgir",
+        id: "1"
+    },
+    {
+        name: "levantarse",
+        id: "2"
+    },
+    {
+        name: "ser",
+        id: "3"
+    },
+    {
+        name: "golpear",
+        id: "4"
+    },
+    {
+        name: "convertirse",
+        id: "5"
+    },
+    {
+        name: "llegar a ser",
+        id: "6"
+    },
+    {
+        name: "comenzar",
+        id: "7"
+    },
+    {
+        name: "apostar",
+        id: "8"
+    },
+    {
+        name: "morder",
+        id: "9"
+    },
+    {
+        name: "sangrar",
+        id: "10"
+    },
+    {
+        name: "soplar",
+        id: "11"
+    },
+    {
+        name: "romper",
+        id: "12"
+    },
+    {
+        name: "traer",
+        id: "13"
+    },
+    {
+        name: "construir",
+        id: "14"
+    },
+    {
+        name: "comprar",
+        id: "15"
+    },
+    {
+        name: "atrapar",
+        id: "16"
+    },
+    {
+        name: "elegir",
+        id: "17"
+    },
+    {
+        name: "venir",
+        id: "18"
+    },
+    {
+        name: "costar",
+        id: "19"
+    },
+    {
+        name: "arrastrarse",
+        id: "20"
+    },
+    {
+        name: "cortar",
+        id: "21"
+    },
+    {
+        name: "dar repartir",
+        id: "22"
+    },
+    {
+        name: "hacer",
+        id: "23"
+    },
+    {
+        name: "dibujar",
+        id: "24"
+    },
+    {
+        name: "soñar",
+        id: "25"
+    },
+    {
+        name: "beber",
+        id: "26"
+    },
+    {
+        name: "conducir",
+        id: "27"
+    },
+    {
+        name: "comer",
+        id: "28"
+    },
+    {
+        name: "caer",
+        id: "29"
+    },
+    {
+        name: "alimentar",
+        id: "30"
+    },
+    {
+        name: "sentir",
+        id: "31"
+    },
+    {
+        name: "pelear",
+        id: "32"
+    },
+    {
+        name: "encontrar",
+        id: "33"
+    },
+    {
+        name: "huir",
+        id: "34"
+    },
+    {
+        name: "volar",
+        id: "35"
+    },
+    {
+        name: "olvidar",
+        id: "36"
+    },
+    {
+        name: "perdonar",
+        id: "37"
+    },
+    {
+        name: "abandonar",
+        id: "38"
+    },
+    {
+        name: "congelar",
+        id: "39"
+    },
+    {
+        name: "tener",
+        id: "40"
+    },
+    {
+        name: "obtener",
+        id: "41"
+    },
+    {
+        name: "dar",
+        id: "42"
+    },
+    {
+        name: "ir",
+        id: "43"
+    },
+    {
+        name: "moler",
+        id: "44"
+    },
+    {
+        name: "crecer",
+        id: "45"
+    },
+    {
+        name: "colgar",
+        id: "46"
+    },
+    {
+        name: "tener",
+        id: "47"
+    },
+    {
+        name: "oír",
+        id: "48"
+    },
+    {
+        name: "esconderse",
+        id: "49"
+    },
+    {
+        name: "golpear",
+        id: "50"
+    },
+    {
+        name: "tener",
+        id: "51"
+    },
+    {
+        name: "mantener",
+        id: "52"
+    },
+    {
+        name: "herir",
+        id: "53"
+    },
+    {
+        name: "doler",
+        id: "54"
+    },
+    {
+        name: "guardar",
+        id: "55"
+    },
+    {
+        name: "arrodillarse",
+        id: "56"
+    },
+    {
+        name: "saber",
+        id: "57"
+    },
+    {
+        name: "encabezar",
+        id: "58"
+    },
+    {
+        name: "aprender",
+        id: "59"
+    },
+    {
+        name: "dejar",
+        id: "60"
+    },
+    {
+        name: "prestar",
+        id: "61"
+    },
+    {
+        name: "dejar",
+        id: "62"
+    },
+    {
+        name: "yacer",
+        id: "63"
+    },
+    {
+        name: "perder",
+        id: "64"
+    },
+    {
+        name: "hacer",
+        id: "65"
+    },
+    {
+        name: "significar",
+        id: "66"
+    },
+    {
+        name: "conocer",
+        id: "67"
+    },
+    {
+        name: "encontrar",
+        id: "68"
+    },
+    {
+        name: "pagar",
+        id: "69"
+    },
+    {
+        name: "poner",
+        id: "70"
+    },
+    {
+        name: "abandonar",
+        id: "71"
+    },
+    {
+        name: "leer",
+        id: "72"
+    },
+    {
+        name: "montar",
+        id: "73"
+    },
+    {
+        name: "ir",
+        id: "74"
+    },
+    {
+        name: "llamar por teléfono",
+        id: "75"
+    },
+    {
+        name: "elevar",
+        id: "76"
+    },
+    {
+        name: "correr",
+        id: "77"
+    },
+    {
+        name: "decir",
+        id: "78"
+    },
+    {
+        name: "ver",
+        id: "79"
+    },
+    {
+        name: "vender",
+        id: "80"
+    },
+    {
+        name: "enviar",
+        id: "81"
+    },
+    {
+        name: "fijar",
+        id: "82"
+    },
+    {
+        name: "coser",
+        id: "83"
+    },
+    {
+        name: "sacudir",
+        id: "84"
+    },
+    {
+        name: "brillar",
+        id: "85"
+    },
+    {
+        name: "disparar",
+        id: "86"
+    },
+    {
+        name: "mostrar",
+        id: "87"
+    },
+    {
+        name: "encoger",
+        id: "88"
+    },
+    {
+        name: "cerrar",
+        id: "89"
+    },
+    {
+        name: "cantar",
+        id: "90"
+    },
+    {
+        name: "hundir",
+        id: "91"
+    },
+    {
+        name: "sentarse",
+        id: "92"
+    },
+    {
+        name: "dormir",
+        id: "93"
+    },
+    {
+        name: "deslizar",
+        id: "94"
+    },
+    {
+        name: "sembrar",
+        id: "95"
+    },
+    {
+        name: "hablar",
+        id: "96"
+    },
+    {
+        name: "deletrear",
+        id: "97"
+    },
+    {
+        name: "gastar",
+        id: "98"
+    },
+    {
+        name: "derramar",
+        id: "99"
+    },
+    {
+        name: "dividir",
+        id: "100"
+    },
+    {
+        name: "estropear",
+        id: "101"
+    },
+    {
+        name: "extenderse",
+        id: "102"
+    },
+    {
+        name: "estar de pie",
+        id: "103"
+    },
+    {
+        name: "robar",
+        id: "104"
+    },
+    {
+        name: "picar",
+        id: "105"
+    },
+    {
+        name: "apestar",
+        id: "106"
+    },
+    {
+        name: "golpear",
+        id: "107"
+    },
+    {
+        name: "jurar",
+        id: "108"
+    },
+    {
+        name: "barrer",
+        id: "109"
+    },
+    {
+        name: "nadar",
+        id: "110"
+    },
+    {
+        name: "tomar",
+        id: "111"
+    },
+    {
+        name: "enseñar",
+        id: "112"
+    },
+    {
+        name: "romper",
+        id: "113"
+    },
+    {
+        name: "decir",
+        id: "114"
+    },
+    {
+        name: "pensar",
+        id: "115"
+    },
+    {
+        name: "lanzar",
+        id: "116"
+    },
+    {
+        name: "pisar",
+        id: "117"
+    },
+    {
+        name: "despertarse",
+        id: "118"
+    },
+    {
+        name: "llevar puesto",
+        id: "119"
+    },
+    {
+        name: "tejer",
+        id: "120"
+    },
+    {
+        name: "llorar",
+        id: "121"
+    },
+    {
+        name: "ganar",
+        id: "122"
+    },
+    {
+        name: "retorcer",
+        id: "123"
+    },
+    {
+        name: "escribir",
+        id: "124"
+    }
+]
